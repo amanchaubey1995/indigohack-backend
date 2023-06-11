3b7ed7c3b5ce006c36aa2c3b90a083e0adf1d3de
amanchaubey1995 <amanchaubey1995@gmail.com>
2023-06-11 06:34:26 +0000

    changes

---
+++ src/indigoCurd.cs
@@ -0,0 +1,13 @@
+
+ namespace IndigoBackEnd
+{
+public Guid itineary;
+    public string FromLocation;
+    public string ToLocation;
+    public string DepartureDate;
+    public string AddReturnTrip;
+    public string NoOfPassanger;
+    public string BookingOn;
+    
+    
+}
---
+++ src/indigoCurdGet.cs
@@ -0,0 +1,14 @@
+ namespace IndigoBackEnd
+{
+public Guid itineary;
+    public string FromLocation;
+    public string ToLocation;
+    public string DepartureDate;
+    public string AddReturnTrip;
+    public string NoOfPassanger;
+    public string BookingOn;
+    
+
+    
+    
+}
